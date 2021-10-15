a5d4dc44c092cb5c175f794a86b46e9ae1ac9faf
Voska Vojtech <vojtech.voska@comap-control.com>
2021-10-15 20:20:17 +0200

    Implement multiThreadedDownload

--- src/Wget.java
+++ src/Wget.java
@@ -2,6 +2,7 @@ import java.util.HashSet;
 
 public class Wget
 {
+    static final Object mutex_seen = new Object();
 
     public static void iterativeDownload(String initialURL)
     {
@@ -28,14 +29,44 @@ public class Wget
         while (!queue.isEmpty())
         {
             String url = queue.dequeue();
-            Xurl.download(url); // don't change this line
+            Xurl.download(url);
         }
     }
 
-    @SuppressWarnings("unused")
     public static void multiThreadedDownload(String initialURL)
     {
-        // to be completed later
+        final URLQueue queue = new SynchronizedListQueue();
+        final HashSet<String> seen = new HashSet<String>();
+
+        // defines a new URLhandler
+        DocumentProcessing.handler = new DocumentProcessing.URLhandler()
+        {
+            // this method will be called for each matched url
+            @Override
+            public void takeUrl(String url)
+            {
+                synchronized (mutex_seen)
+                {
+                    if (seen.add(url))
+                        queue.enqueue(url);
+                }
+            }
+        };
+
+        // to start, we push the initial url into the queue
+        DocumentProcessing.handler.takeUrl(initialURL);
+        int start_thread_cnt = Thread.activeCount();
+        while (true)
+        {
+            if (!queue.isEmpty())
+            {
+                String url = queue.dequeue();
+                Thread thread = new Thread(new XurlThread(url));
+                thread.start();
+            }
+            else if (Thread.activeCount() - start_thread_cnt <= 0)
+                break;
+        }
     }
 
     @SuppressWarnings("unused")
@@ -51,6 +82,23 @@ public class Wget
           System.err.println("Usage: java Wget url");
           System.exit(-1);
         }
-        iterativeDownload(args[0]);
+//        iterativeDownload(args[0]);
+        multiThreadedDownload(args[0]);
+    }
+}
+
+class XurlThread implements Runnable
+{
+    String url;
+
+    public XurlThread(String url)
+    {
+        this.url = url;
+    }
+
+    @Override
+    public void run()
+    {
+        Xurl.download(url);
     }
 }
