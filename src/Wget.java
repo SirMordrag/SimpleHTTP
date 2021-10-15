2071b181d89e4691bfd4de0eccb37d9d91898320
Voska Vojtech <vojtech.voska@comap-control.com>
2021-10-15 22:31:09 +0200

    Implement threadPoolDownload

--- src/Wget.java
+++ src/Wget.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.concurrent.TimeUnit;
 
 public class Wget
 {
     static final Object mutex_seen = new Object();
+    static boolean use_in_band_signaling = true;
 
     public static void iterativeDownload(String initialURL)
     {
@@ -16,11 +19,8 @@ public class Wget
             @Override
             public void takeUrl(String url)
             {
-                if (!seen.contains(url))
-                {
+                if (seen.add(url))
                     queue.enqueue(url);
-                    seen.add(url);
-                }
             }
         };
 
@@ -69,10 +69,61 @@ public class Wget
         }
     }
 
-    @SuppressWarnings("unused")
     public static void threadPoolDownload(int poolSize, String initialURL)
     {
-        // to be completed later
+        final URLQueue queue = new BlockingListQueue();
+        final HashSet<String> seen = new HashSet<String>();
+
+        // defines a new URLhandler
+        DocumentProcessing.handler = new DocumentProcessing.URLhandler()
+        {
+            // this method will be called for each matched url
+            @Override
+            public void takeUrl(String url)
+            {
+                synchronized (mutex_seen)
+                {
+                    if (seen.add(url))
+                        queue.enqueue(url);
+                }
+            }
+        };
+
+        // to start, we push the initial url into the queue
+        DocumentProcessing.handler.takeUrl(initialURL);
+        ArrayList<Thread> thread_list = new ArrayList<>();
+
+        // start threads
+        for (int i = 0; i < poolSize; i++)
+        {
+            Thread thread = new Thread(new XurlPoolThread(queue));
+            thread.start();
+            thread_list.add(thread);
+        }
+
+        // wait for finish
+        int strike_cnt = 0;
+        while(strike_cnt < 5)
+        {
+            int blocked_cnt = 0;
+            for (Thread thread : thread_list)
+                if (thread.getState() == Thread.State.WAITING)
+                    blocked_cnt++;
+            if (blocked_cnt == poolSize)
+                strike_cnt++;
+            else
+                strike_cnt = 0;
+            sleep(20);
+        }
+
+        // end threads
+        for (Thread thread : thread_list)
+            if (use_in_band_signaling)
+                thread.interrupt();
+            else
+                queue.enqueue("**STOP**");
+
+        sleep(500);
     }
 
     public static void main(String[] args)
@@ -82,8 +133,15 @@ public class Wget
           System.err.println("Usage: java Wget url");
           System.exit(-1);
         }
+        
 //        iterativeDownload(args[0]);
-        multiThreadedDownload(args[0]);
+//        multiThreadedDownload(args[0]);
+        threadPoolDownload(Integer.parseInt(args[0]), args[1]);
+    }
+
+    static void sleep(int milliseconds)
+    {
+        try {TimeUnit.MILLISECONDS.sleep(milliseconds);} catch (InterruptedException ignored){}
     }
 }
 
@@ -100,5 +158,27 @@ class XurlThread implements Runnable
     public void run()
     {
         Xurl.download(url);
+    }
+}
+
+class XurlPoolThread implements Runnable
+{
+    URLQueue queue;
+
+    public XurlPoolThread(URLQueue queue)
+    {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run()
+    {
+        while(true)
+        {
+            String url = queue.dequeue();
+            if (url.equals("**STOP**") || Thread.interrupted())
+                break;
+            Xurl.download(url);
+        }
     }
 }
