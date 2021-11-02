2e77e62a5f90a15a83ffa160112ce0182db9c372
Vojtěch Voska <voskac@seznam.cz>
2021-11-02 11:43:36 +0100

    Xserver fixes

--- src/Xserver.java
+++ src/Xserver.java
@@ -3,10 +3,15 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -21,8 +26,7 @@ public class Xserver {
     BlockingQueue<Socket> client_queue;
 
     public Xserver(int port, String root, int pool_size) {
-        Socket client_socket = null;
-        client_queue = new ArrayBlockingQueue<Socket>(10);
+        client_queue = new LinkedBlockingQueue<>();
         try {
             xsrv_socket = new ServerSocket(port);
             xsrv_socket.setSoTimeout(timeout);
@@ -30,52 +34,39 @@ public class Xserver {
             error("Failed to open Server on port " + port, e.getMessage());
             System.exit(-1);
         }
-        while (true) {
-            // establish a socket
 
-
-            // start threads
-            thread_list = new ArrayList<>();
-            for (int i = 0; i < pool_size; i++) {
-                Thread thread = new Thread(new XserverThread(client_queue));
-                thread.start();
-                thread_list.add(thread);
-            }
-
-            // main loop
-            do {
-                try {
-                    client_socket = xsrv_socket.accept();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } while (client_socket == null);
-
-            handleConnection(client_socket);
-
-//        while (strike_cnt < 5) {
-//            try {
-////                Socket client_socket = xsrv_socket.accept();
-//                handleConnection(xsrv_socket.accept());
-//            } catch (IOException e) {
-//                error("Failed to listen to connection", e.getMessage());
-//            }
-//
-//            if (isFinished())
-//                break;
-//            sleep(20);
-//        }
-
-            // end threads
-            for (Thread thread : thread_list)
-                thread.interrupt();
-//            try {
-//                xsrv_socket.close();
-//            } catch (IOException e) {
-//                error("Failed to close server", e.getMessage());
-//            }
-//            sleep(500);
+        // start threads
+        thread_list = new ArrayList<>();
+        for (int i = 0; i < pool_size; i++)
+        {
+            Thread thread = new Thread(new XserverThread(client_queue, root));
+            thread.start();
+            thread_list.add(thread);
         }
+
+        while (true)
+        {
+            try
+            {
+                Socket client_socket = xsrv_socket.accept();
+                if (client_socket != null)
+                    handleConnection(client_socket);
+            } catch (SocketTimeoutException e) {
+                break;
+            } catch (IOException e) {
+                error("Failed to listen to connection", e.getMessage());
+            }
+        }
+
+        // end threads
+        for (Thread thread : thread_list)
+            thread.interrupt();
+        try {
+            xsrv_socket.close();
+        } catch (IOException e) {
+            error("Failed to close server", e.getMessage());
+        }
+        sleep(500);
     }
 
     void handleConnection(Socket socket) {
@@ -87,8 +78,8 @@ public class Xserver {
     }
 
     boolean isFinished() {
-        if (DEBUG)
-            return false;
+//        if (DEBUG)
+//            return false;
 
         int blocked_cnt = 0;
         for (Thread thread : thread_list)
@@ -161,18 +152,20 @@ public class Xserver {
 
 class XserverThread implements Runnable {
     BlockingQueue<Socket> queue;
+    String root;
 
-    public XserverThread(BlockingQueue<Socket> queue) {
+    public XserverThread(BlockingQueue<Socket> queue, String root)
+    {
         this.queue = queue;
+        this.root = root;
     }
 
     @Override
     public void run() {
         while (true) {
             try {
-                System.out.println(queue.size());
                 Socket socket = queue.take();
-                new XserverProcessor(socket);
+                new XserverProcessor(socket, root);
             } catch (InterruptedException e) {
                 break;
             }
@@ -189,46 +182,47 @@ class XserverProcessor {
     List<String> req_header;
     String path;
     String file;
-    int response_ok;
+    int response_ok = 200;
     Boolean file_exist;
-    public XserverProcessor(Socket client_socket) {
+    String root_dir;
+
+
+    public XserverProcessor(Socket client_socket, String root) {
         socket = client_socket;
+        root_dir = root;
 
         openConnection();
 
-        processRequest();
-//        if (!response_ok) {
-//            if (socket.isClosed())
-//                break;
-//        }
+        do
+        {
+            processRequest();
+            sleep(100);
+        }
+        while (!socket.isClosed() && (response_ok == 200 || response_ok == 404));
+
         closeConnection();
     }
 
-    int processRequest() {
+    void processRequest() {
         file_exist = true;
         response_ok = 200;
-        readHeader();
+        if (!readHeader())
+        {
+            response_ok = 600; // connection closed
+            return;
+        }
         if (!analyzeHeader()) {
             path = ":tag:bad_request";
             response_ok = 400;
             debug("BAD REQUEST");
         }
-        if (!getFile()) {
-//            if (response_ok!=200)
-            if (response_ok!=400) {
-                response_ok = 404;
-                file = message404();
-
-            }
-        }
+        getFile();
 
         writeResponse(response_ok);
-
-        return response_ok;
     }
 
 
-    void readHeader() {
+    boolean readHeader() {
         req_header = new ArrayList<>();
         try {
             for (int i = 0; ; i++) {
@@ -236,24 +230,27 @@ class XserverProcessor {
                 debug(line);
                 req_header.add(line + "\r\n");
 
+                if (line == null)
+                    return false;
                 if (line.equals(""))
                     break;
             }
         } catch (IOException e) {
-            error("Failed to read page", e.getMessage());
+            error("Failed to read header", e.getMessage());
         }
+        return true;
     }
 
     boolean analyzeHeader() {
-        Pattern r = Pattern.compile("^(GET) (/[^ ]*) (HTTP/1.1)$");
+        Pattern r = Pattern.compile("(GET) (/[^ ]*) (HTTP/1.1)$");
         Matcher m = r.matcher(req_header.get(0));
 
-        debug("HERE IS HEARDER");
+        debug("HERE IS HEADER");
         // http request
         if (!m.find()) {
             return false;
         }
-
+        debug("HERE IS HEADER");
         // path
         path = m.group(2);
 
@@ -266,26 +263,41 @@ class XserverProcessor {
     }
 
     boolean getFile() {
-        if (path.equals(":tag:bad_request")) {
+        debug("Path: " + path);
+        if (path.equals(":tag:bad_request"))
+        {
             file = message400();
+            response_ok = 400;
             return false;
-        } else if (path.equals("/")) {
+        }
+        else if (path.equals("/")) {
             file = messageIndex();
             return true;
         }
+        else
+        {
+            path = root_dir + path;
+        }
+        debug("Full Path: " + path);
 
         try {
-            File req_file = new File(path);
-            file_reader = new BufferedReader(new FileReader(req_file));
+//            File req_file = new File(path);
+//            file_reader = new BufferedReader(new FileReader(path));
+//
+//            String line;
+//            file = "";
+//            while (file_reader.ready())
+//            {
+//                line = file_reader.readLine();
+//                debug(line);
+//                file += (line + "\r\n");
+//            }
+            file = new String(Files.readAllBytes(Paths.get(path)), StandardCharsets.UTF_8);
 
-            String line;
-            file = "";
-            while ((line = reader.readLine()) != null)
-                file += line;
-
-        } catch (IOException e) {
-            return false;
-//            error("Failed to read file", e.getMessage());
+        } catch (IOException e)
+        {
+            file = message404();
+            response_ok = 404;
         }
         return true;
     }
@@ -321,7 +333,8 @@ class XserverProcessor {
             socket.close();
             reader.close();
             writer.close();
-            if (response_ok==200) file_reader.close();
+            if (file_reader != null)
+                file_reader.close();
             debug("Connection Closed");
         } catch (IOException e) {
             error("Failed to close connection", e.getMessage());
